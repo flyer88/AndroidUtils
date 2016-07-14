66372ac9b936951a1bf8bd49e7111cee8c475cb9
dove <dove@dovedeMac-mini.local>
2016-07-14 10:32:26 +0800

    修复 demo中的使用

--- Utils/app/src/main/java/com/dove/utils/NextActivity.java
+++ Utils/app/src/main/java/com/dove/utils/NextActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.dove.androidutils.common.CommonUtils;
+import com.dove.androidutils.ui.ScreenUtils;
 
 /**
  * Created by dove on 16/7/13.
@@ -13,6 +14,6 @@ public class NextActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CommonUtils.getScreenHeight();
+        ScreenUtils.getScreenHeight();
     }
 }
